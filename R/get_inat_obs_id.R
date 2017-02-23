ebe8483d016c110129e4291e135c7d20757af193
luismurao <luismurao@gmail.com>
2017-02-23 13:33:12 -0600

    project

---
+++ R/get_inat_obs_id.R
@@ -0,0 +1,16 @@
+#' Get information on a specific observation
+#'
+#' @export
+#' @import httr plyr jsonlite
+#' @param id a single id for an inaturalist observation record
+#' @return a list with full details on a given record
+#' @examples \dontrun{
+#' m_obs <- get_inat_obs(query="Monarch Butterfly")
+#' get_inat_obs_id(m_obs$id[1])
+#' }
+get_inat_obs_id <- function(id){
+  base_url <- "http://www.naturalista.mx/"
+  q_path <- paste("observations/",as.character(id),".json", sep = "")
+  fromJSON(content(GET(base_url,path = q_path), as = "text"))
+}
+

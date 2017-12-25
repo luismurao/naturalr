b91513d83d6c3ddc61101d48246638051733e3c3
luismurao <luismurao@gmail.com>
2017-12-24 20:50:04 -0600

    Image preview option. Displays the photo of the record if it exists .

--- R/inat_map.R
+++ R/inat_map.R
@@ -2,37 +2,59 @@
 #'
 #' @description Plot observations from iNaturalist for Mexico.
 #' @param data data frame of iNaturalist observations
-#'
+#' @param imagePreview Logical if TRUE the image of the record will be shown.
 #' @return a leaflet map object
 #' @examples \dontrun{
-#'   m_obs <- get_inat_obs(taxon="Ambystoma maculatum")
-#'   salamander_map <- inat_map(m_obs,plot=FALSE)
-#'   ### Now we can modify the returned map
-#'   salamander_map + borders("state") + theme_bw()
+#' puma_occs <- get_inat_obs(query = "Puma concolor",
+#'                           geo = T,maxresults = 10000,
+#'                          captive_cultivated = F)
+#' inat_map(puma_occs)
+#'
 #' }
 #' @import leaflet leaflet
 #' @export
 
-inat_map <- function(data){
+inat_map <- function(data,imgPreview=T){
   if(is.data.frame(data)){
     data$sp_info <- paste0("https://es.wikipedia.org/wiki/",
                            gsub(x = data$scientific_name[1],pattern = " ",
                                 replacement = "_"))
-    data$data_rowID <- paste("<b>Species: </b><a href='",data$sp_info,"'>",
-                             data$scientific_name,"</a><br/>",
-                             "<b>rowID:</b>",1:dim(data)[1],
-                             "<br/><b>observed on: </b>",
-                             data$observed_on,
-                             "<br/><b>captive or cultivated:</b>",
-                             data$captive_cultivated,
-                             "<br/><b>Record url: </b><a href='",data$url,
-                             "'>click</a>",
-                             "<br/><b>Image url: </b><a href='",data$image_url,
-                             "'>click, </a><br/>")
+    if(imgPreview){
+      data$data_rowID <- paste("<b>Species: </b><a href='",data$sp_info,"'>",
+                               data$scientific_name,"</a><br/>",
+                               "<b>rowID:</b>",1:dim(data)[1],
+                               "<br/><b>observed on: </b>",
+                               data$observed_on,
+                               "<br/><b>captive or cultivated:</b>",
+                               data$captive_cultivated,
+                               "<br/><b>Record url: </b><a href='",data$url,
+                               "'>click</a>",
+                               "<br/><b>Image preview:<br/></b><a href='",
+                               data$image_url,
+                               "'><div style='text-align: center;'><img src='",
+                               data$image_url,
+                               "' height='72' width='72' align='middle'></div></a>")
+    }
+    else{
+      data$data_rowID <- paste("<b>Species: </b><a href='",data$sp_info,"'>",
+                               data$scientific_name,"</a><br/>",
+                               "<b>rowID:</b>",1:dim(data)[1],
+                               "<br/><b>observed on: </b>",
+                               data$observed_on,
+                               "<br/><b>captive or cultivated:</b>",
+                               data$captive_cultivated,
+                               "<br/><b>Record url: </b><a href='",data$url,
+                               "'>click</a>",
+                               "<br/><b>Image url: </b><a href='",data$image_url,
+                               "'>click, </a>")
+    }
+
     m <- leaflet(data) %>%
       addTiles() %>%  # Add default OpenStreetMap map tiles
-      addMarkers(lng=~longitude, lat=~latitude, popup= ~data_rowID)
-    return(m)  # Pr
+      addMarkers(lng=~longitude, lat=~latitude,
+                 popup= ~data_rowID);
+    return(m)
+
   }
 
 }
